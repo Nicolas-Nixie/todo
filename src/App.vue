326314f052f825f2bcc346f079f2f3858aff2608
unknown <mkleinnicolas@gmail.com>
2022-04-11 16:56:20 +0200

    second

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    {{ todos }}
+    <todos v-model="todos"></todos>
+  </div>
+
+</template>
+
+<script>
+
+import Todos from './components/Todos.vue'
+
+export default {
+
+  data () {
+    return {
+      todos: [{
+        name: 'Demo',
+        completed: true
+      }]
+    }
+  },
+
+  methods: {
+    addTodo () {
+      this.todos.push({
+        name: 'Jean',
+        completed: false
+      })
+    }
+  },
+
+  components: {
+    Todos
+  },
+  name: 'App'
+
+}
+</script>
+
+<style>
+/*
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*/
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,150 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+
+            <h1>Todos</h1>
+            <span class="todo-count"><strong>{{ completed }}</strong> tâches faites</span>
+            <input type="test" class="new-todo"  placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+
+        </header>
+
+        <div class="main">
+            <input type="checkbox" class="toggle-all" v-model="allDone">
+            <ul class="todo-list">
+                <li class="todo" v-for="(todo, index) in filteredTodos" :key="index" :class="{completed: todo.completed, editing: todo === editing}">
+                    <div class="view">
+                        <input type="checkbox" v-model="todo.completed" class="toggle">
+                        <label @dblclick="editing(todo)" >{{ todo.name }}</label>
+                        <p v-show="todo.completed"></p>
+                        <button class="destroy" @click.prevent="deletTodo(todo)"></button>
+                    </div>
+                    <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit" @blur="doneEdit" @keyup.esc="cancelEdit" v-focus="todo === editing">
+                </li>
+            </ul>
+        </div>
+        <footer class="footer" v-show="hasTodos">
+            <span class="todo-count"><strong>{{ remaining }}</strong> tâches à faire</span>
+            <ul class="filters">
+                <li><a href="#" class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+                <li><a href="#" class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+                <li><a href="#" class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+            </ul>
+            <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted">Supprimer les tâhces finis</button>
+        </footer>
+    </section>
+</template>
+
+<script>
+
+import Vue from 'vue'
+
+export default {
+  props: {
+    value: {type: Array, default () { return [] }}
+  },
+
+  data () {
+    return {
+      todos: this.value,
+      newTodo: '',
+      filter: 'all',
+      editing: null,
+      oldTodo: '',
+      pending: ''
+    }
+  },
+
+  watcher: {
+    value (value) {
+      this.todos = value
+    }
+  },
+
+  methods: {
+    addTodo () {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      })
+
+      this.newTodo = ''
+    },
+
+    deleteTodo (todo) {
+      this.todos = this.todos.filter(i => i !== todo)
+      this.$emit('input', this.todos)
+    },
+
+    deleteCompleted () {
+      this.todos = this.todos.filter(todo => !todo.completed)
+      this.$emit('input', this.todos)
+    },
+
+    editTodo (todo) {
+      this.editing = todo
+      this.oldTodo = todo.name
+    },
+
+    doneEdit () {
+      this.editing = null
+    },
+
+    cancelEdit () {
+      this.editing.name = this.oldTodo
+      this.doneEdit()
+    }
+  },
+
+  computed: {
+    allDone: {
+      get () {
+        return this.remaining === 0
+      },
+      set (value) {
+        this.todos.forEach(todo => {
+          todo.completed = value
+        })
+      }
+    },
+
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
+    },
+
+    completed () {
+      const test = this.todos.filter(function (todo) {
+        return todo.completed
+      })
+      return test.length
+    },
+
+    hasTodos () {
+      return this.todos.length > 0
+    },
+
+    filteredTodos () {
+      if (this.filter === 'todo') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter === 'done') {
+        return this.todos.filter(todo => todo.completed)
+      }
+
+      return this.todos
+    }
+  },
+
+  directives: {
+    focus (el, value) {
+      if (value) {
+        Vue.nextTick(_ => {
+          el.focus()
+        })
+      }
+    }
+  }
+
+}
+
+</script>
+
+<style src="./todos.css"></style>
